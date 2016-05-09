343f601e835461ee5508444d2a765a7b71f0c1fa
alickyao <7181210@qq.com>
2016-05-09 10:42:54 +0800

    ok

--- NewCyclone/Models/SysDoc.cs
+++ NewCyclone/Models/SysDoc.cs
@@ -7,26 +7,116 @@ using NewCyclone.DataBase;
 namespace NewCyclone.Models
 {
     /// <summary>
-    /// 基础文档
+    /// 系统基础文档
     /// </summary>
     public class SysDoc
     {
-        public SysDoc() {
+
+        /// <summary>
+        /// 文档ID
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// 创建者
+        /// </summary>
+        public SysUser createdBy { get; set; }
+
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime createdOn { get; set; }
+
+        /// <summary>
+        /// 修改者
+        /// </summary>
+        public SysUser modifiedBy { get; set; }
+
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime modifiedOn { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string caption { get; set; }
+
+        /// <summary>
+        /// 是否已被标记为删除
+        /// </summary>s
+        public bool isDeleted { get; set; }
+
+
+        private List<SysCatTree> _cat = new List<SysCatTree>();
+
+        /// <summary>
+        /// 所属在类别
+        /// </summary>
+        public List<SysCatTree> cat {
+            get { return _cat; }
+            set { _cat = value; }
+        }
+
+        /// <summary>
+        /// 使用ID构造文档基础项
+        /// </summary>
+        /// <param name="id"></param>
+        public SysDoc(string id) {
             
         }
     }
 
+    /// <summary>
+    /// 网站文档
+    /// </summary>
+    public class WebDoc : SysDoc
+    {
+        /// <summary>
+        /// 所属的功能模块描述
+        /// </summary>
+        public VMComboBox fun { get; set; }
+        /// <summary>
+        /// 文档描述
+        /// </summary>
+        public string describe { get; set; }
 
+        /// <summary>
+        /// 使用ID构造一个网站文档
+        /// </summary>
+        /// <param name="id"></param>
+        public WebDoc(string id) : base(id)
+        {
+
+        }
+    }
 
     /// <summary>
-    /// 带图集的文档
+    /// 网页页面（图文信息）
     /// </summary>
-    public class SysDocImgRote : SysDoc {
+    public class WebDocPage : WebDoc {
+
         /// <summary>
-        /// 构造方法
+        /// seo标题
         /// </summary>
-        public SysDocImgRote() {
-            
+        public string seoTitle { get; set; }
+
+        /// <summary>
+        /// seo关键字
+        /// </summary>
+        public string seoKeyWords { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string content { get; set; }
+
+        /// <summary>
+        /// 使用ID构造网页页面文档（图文信息）
+        /// </summary>
+        /// <param name="id"></param>
+        public WebDocPage(string id) : base(id) {
+
         }
     }
 }
