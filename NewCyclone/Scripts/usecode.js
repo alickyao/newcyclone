1a45c8ecef167d9728ee9c83aa3307b34dbc675a
alickyao <7181210@qq.com>
2016-05-18 07:18:01 +0800

    0518 morning

--- NewCyclone/Scripts/usecode.js
+++ NewCyclone/Scripts/usecode.js
@@ -2,18 +2,52 @@
 **时间格式化
 */
 Date.prototype.Format = function (fmt) { //author: meizz 
-    var o = {
-        "M+": this.getUTCMonth() + 1, //月份 
-        "d+": this.getUTCDate(), //日 
-        "h+": this.getUTCHours(), //小时 
-        "m+": this.getUTCMinutes(), //分 
-        "s+": this.getUTCSeconds(), //秒 
-        "q+": Math.floor((this.getUTCMonth() + 3) / 3), //季度 
-        "S": this.getUTCMilliseconds() //毫秒 
-    };
+
+    var a = (("ActiveXObject" in window) || navigator.userAgent.toLowerCase().indexOf("edge") > 0); //true 表示IE
+    var o = {};
+    if (a) {
+        o = {
+            "M+": this.getMonth() + 1, //月份 
+            "d+": this.getDate(), //日 
+            "h+": this.getHours(), //小时 
+            "m+": this.getMinutes(), //分 
+            "s+": this.getSeconds(), //秒 
+            "q+": Math.floor((this.getUTCMonth() + 3) / 3), //季度 
+            "S": this.getMilliseconds() //毫秒 
+        }
+    }
+    else {
+        o = {
+            "M+": this.getUTCMonth() + 1, //月份 
+            "d+": this.getUTCDate(), //日 
+            "h+": this.getUTCHours(), //小时 
+            "m+": this.getUTCMinutes(), //分 
+            "s+": this.getUTCSeconds(), //秒 
+            "q+": Math.floor((this.getUTCMonth() + 3) / 3), //季度 
+            "S": this.getUTCMilliseconds() //毫秒 
+        }
+    }
+    //var o = {
+    //    "M+": this.getUTCMonth() + 1, //月份 
+    //    "d+": this.getUTCDate(), //日 
+    //    "h+": this.getUTCHours(), //小时 
+    //    "m+": this.getUTCMinutes(), //分 
+    //    "s+": this.getUTCSeconds(), //秒 
+    //    "q+": Math.floor((this.getUTCMonth() + 3) / 3), //季度 
+    //    "S": this.getUTCMilliseconds() //毫秒 
+
+    //    //"M+": this.getMonth() + 1, //月份 
+    //    //"d+": this.getDate(), //日 
+    //    //"h+": this.getHours(), //小时 
+    //    //"m+": this.getMinutes(), //分 
+    //    //"s+": this.getSeconds(), //秒 
+    //    //"q+": Math.floor((this.getUTCMonth() + 3) / 3), //季度 
+    //    //"S": this.getMilliseconds() //毫秒 
+    //};
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getUTCFullYear() + "").substr(4 - RegExp.$1.length));
     for (var k in o)
         if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    //fmt = this.toLocaleString();
     return fmt;
 }
 
