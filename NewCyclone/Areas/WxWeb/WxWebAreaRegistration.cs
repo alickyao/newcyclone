08cfa6fb016fafe579cd89daa1a0f18a5de65083
alickyao <7181210@qq.com>
2016-05-26 17:15:08 +0800

    0526

---
+++ NewCyclone/Areas/WxWeb/WxWebAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NewCyclone.Areas.WxWeb
+{
+    public class WxWebAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "WxWeb";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "WxWeb_default",
+                "wx/{controller}/{action}/{id}",
+                new { controller = "Response", action = "response", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
