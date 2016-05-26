544954b7a3b588ab7af0d8415745c4fb38589632
alickyao <7181210@qq.com>
2016-05-26 17:29:51 +0800

    0526

---
+++ NewCyclone/Areas/WxWeb/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewCyclone.Areas.WxWeb.Controllers
+{
+    /// <summary>
+    /// 授权 与 用户
+    /// </summary>
+    public class AuthController : Controller
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
