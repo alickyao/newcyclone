8680c9e7d998df487164d5f48e60ce815095e763
alickyao <7181210@qq.com>
2016-05-30 17:32:10 +0800

    0530

---
+++ NewCyclone/Areas/Admin/Controllers/WeiXinController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewCyclone.Areas.Admin.Controllers
+{
+    /// <summary>
+    /// 微信基础模块
+    /// </summary>
+    public class WeiXinController : MBaseController
+    {
+        // GET: Admin/WeiXin
+
+        
+        /// <summary>
+        /// 菜单按钮类型说明
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult menuButtonDescribe()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 菜单管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult menu()
+        {
+            setPageId();
+            return View();
+        }
+    }
+}
